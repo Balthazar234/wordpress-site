8946720e982f82c74b93afc86ed1b7db7749a86e
root <root@ip-172-31-82-42.ec2.internal>
2024-07-23 10:25:01 +0000

    update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  */
 
 // Add a custom message
-echo "<h1 style='text-align: center; color: blue;'>Hello, this is a test change!</h1>";
+echo "<h1 style='text-align: center; color: blue;'>Hello, this is a test change no.2!</h1>";
 
 /**
  * Tells WordPress to load the WordPress theme and output it.
